195b33ee44cb7b0a9b0d2223186601bc908afb53
riyad1234 <rh3510594@gmail.com>
2023-08-29 18:39:27 +0600

    finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/logo.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='header'>
             <img className='logo' src={logo} alt="" />
            <nav>
-           <a href="/Shop">shop</a>
-                <a href="/order">order</a>
-                <a href="/inventory">inventory</a>
-                <a href="/login">login</a>
+                <Link to="/order">order</Link>
+                <Link to="/inventory">inventory</Link>
+                <Link to="/">shop</Link>
+                <Link to="/login">login</Link>
            </nav>
                 
         </div>
